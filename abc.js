59d8c7ed6876a418cd3676835646f7c25f383d64
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-30 21:19:03 -0500

    Move cloak_btn declaration inside window.onload

--- abc.js
+++ abc.js
@@ -1,10 +1,9 @@
-let cloak_btn = document.querySelector("button");
-
 function cloak(url){
   var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999");
   w.document.write(`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none;" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${url}"></iframe>`);
 }
 
-cloak_btn.onclick = () => {
-  cloak("https://mini-mafia.github.io/");
-};
+window.onload = () => {
+  let cloak_btn = document.querySelector("button");
+  cloak_btn.onclick = () => cloak("https://mini-mafia.github.io/");
+}
