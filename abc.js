aad3329f2f4dc5df462f9d177f7ea45077095c5d
Felix Schwiebert <fschwieb@protonmail.com>
2026-01-05 16:02:59 -0500

    Change iframe dimensions to style properties

--- abc.js
+++ abc.js
@@ -3,8 +3,8 @@ function cloak() {
   w.document.body.style.margin = "0";
   
   let iframe = w.document.createElement('iframe');
-  iframe.height = "100dvh";
-  iframe.width = "100vw";
+  iframe.style.height = "100dvh";
+  iframe.style.width = "100vw";
   iframe.style.border = "none";
   iframe.referrerpolicy = "no-referrer";
   iframe.src = "https://mini-mafia.github.io";
